71ea267c506a7f33a16cd8d4f6a7f8b6f4bb86f6
Jaesung Hong <jezsung@gmail.com>
2022-10-17 23:18:49 -0500

    add unit test UserBusinessLogic.FindById success

---
+++ FakeUserManager.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using SD_340_W22SD_Final_Project_Group6.Models;
+
+namespace SD_125_W22SD_UnitTest
+{
+    public class FakeUserManager : UserManager<ApplicationUser>
+    {
+        public FakeUserManager()
+            : base(new Mock<IUserStore<ApplicationUser>>().Object,
+                new Mock<IOptions<IdentityOptions>>().Object,
+                new Mock<IPasswordHasher<ApplicationUser>>().Object,
+                new IUserValidator<ApplicationUser>[0],
+                new IPasswordValidator<ApplicationUser>[0],
+                new Mock<ILookupNormalizer>().Object,
+                new Mock<IdentityErrorDescriber>().Object,
+                new Mock<IServiceProvider>().Object,
+                new Mock<ILogger<UserManager<ApplicationUser>>>().Object)
+        { }
+
+        public static UserManager<ApplicationUser> GetFakeUserManager(List<ApplicationUser> users)
+        {
+            var fakeUserManager = new Mock<FakeUserManager>();
+
+            fakeUserManager.Setup(x => x.Users).Returns(users.AsQueryable());
+            fakeUserManager.Setup(x => x.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
+            fakeUserManager.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync((string id) => users.Find(u => u.Id == id));
+            fakeUserManager.Setup(x => x.UpdateAsync(It.IsAny<ApplicationUser>())).ReturnsAsync(IdentityResult.Success);
+            fakeUserManager.Setup(x => x.DeleteAsync(It.IsAny<ApplicationUser>())).ReturnsAsync(IdentityResult.Success);
+            fakeUserManager.Setup(x => x.ChangeEmailAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
+
+            return fakeUserManager.Object;
+        }
+    }
+}
---
+++ UserBusinessLogicTest.cs
@@ -0,0 +1,27 @@
+using Moq;
+using SD_340_W22SD_Final_Project_Group6.BLL;
+using SD_340_W22SD_Final_Project_Group6.Models;
+
+namespace SD_125_W22SD_UnitTest
+{
+    [TestClass]
+    public class UserBusinessLogicTest
+    {
+        [TestMethod]
+        public async Task ShouldReturnCorrectUserWhenMatchingIdPassed()
+        {
+            var user = new ApplicationUser
+            {
+                UserName = "User1",
+                Id = "UserId1",
+                Email = "user1@jello.com"
+            };
+            var userManager = FakeUserManager.GetFakeUserManager(new List<ApplicationUser> { user });
+            var userBusinessLogic = new UserBusinessLogic(userManager);
+
+            var result = await userBusinessLogic.FindById(user.Id);
+
+            Assert.AreEqual(user.Id, result.Id);
+        }
+    }
+}
